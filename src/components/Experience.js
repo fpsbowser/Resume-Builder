f757b416154a301917945b4f79b29cf5a9a2ee60
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-06-24 22:19:05 -0400

    addinputs function created

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,56 +1,72 @@
 import React, { Component } from 'react';
+import uniqid from 'uniqid';
 
 class Experience extends Component {
   constructor(props) {
     super(props);
-    this.state = { experience: [{}, {}, {}] };
+    this.state = {
+      experience: [props.props[0], props.props[1], props.props[2]],
+    };
+    this.addInputs = this.addInputs.bind(this);
   }
 
-  addInputs() {}
+  addInputs() {
+    this.setState((prevState) => ({
+      experience: [
+        ...prevState.experience,
+        {
+          company: '',
+          position: '',
+          start: '',
+          end: '',
+          description: '',
+        },
+      ],
+    }));
+  }
 
   render() {
-    const { company, position, start, end, description } = this.props;
+    // const { company, position, start, end, description } = this.props;
     return (
       <div>
         <button onClick={this.addInputs}>Add</button>
-        <form>
-          <input
-            type="text"
-            placeholder="Company"
-            value={company}
-            name="company"
-            onChange={this.props.handlechange}
-          />
-          <input
-            type="text"
-            placeholder="Position"
-            value={position}
-            name="position"
-            onChange={this.props.handlechange}
-          />
-          <input
-            type="text"
-            placeholder="Start Date"
-            value={start}
-            name="start"
-            onChange={this.props.handlechange}
-          />
-          <input
-            type="text"
-            placeholder="End Date"
-            value={end}
-            name="end"
-            onChange={this.props.handlechange}
-          />
-          <input
-            type="text"
-            placeholder="Description"
-            value={description}
-            name="description"
-            onChange={this.props.handlechange}
-          />
-          <button onClick={this.props.handledelete}>Delete</button>
-        </form>
+        {this.state.experience.map((el) => {
+          return (
+            <div key={uniqid()}>
+              <input
+                value={el.company}
+                name={'company'}
+                placeholder={'Company'}
+                onChange={this.props.handlechange}
+              />
+              <input
+                value={el.position}
+                name={'position'}
+                placeholder={'Position'}
+                onChange={this.props.handlechange}
+              />
+              <input
+                value={el.start}
+                name={'start'}
+                placeholder={'Start'}
+                onChange={this.props.handlechange}
+              />
+              <input
+                value={el.end}
+                name={'end'}
+                placeholder={'End'}
+                onChange={this.props.handlechange}
+              />
+              <input
+                value={el.description}
+                name={'description'}
+                placeholder={'Description'}
+                onChange={this.props.handlechange}
+              />
+            </div>
+          );
+        })}
+        <button onClick={this.props.handledelete}>Delete</button>
       </div>
     );
   }
