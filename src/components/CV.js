7430190c8e49c443012f90afb4a3f7738bfe4cfd
Dakota <prattdakota12@gmail.com>
2022-07-07 14:39:54 -0400

    refactored

--- src/components/CV.js
+++ src/components/CV.js
@@ -2,44 +2,44 @@ import React from 'react';
 import '../styles/CV.css';
 
 const CV = (props) => {
-  const information = props.props.information;
-  const experiences = props.props.experience;
-  const education = props.props.education;
+  const information = props.information;
+  const experiences = props.experience;
+  const education = props.education;
   return (
-    <div className="rendered-resume">
-      <div className="rendered-header-container">
-        <div className="header">
-          <h1 id="name">{information.name}</h1>
-          <h2 id="title">{information.title}</h2>
+    <div className='rendered-resume'>
+      <div className='rendered-header-container'>
+        <div className='header'>
+          <h1 id='name'>{information.name}</h1>
+          <h2 id='title'>{information.title}</h2>
         </div>
-        <div className="contact-information">
-          <p id="phone">{information.phone}</p>
-          <p id="email">{information.email}</p>
-          <p id="location">{information.location}</p>
+        <div className='contact-information'>
+          <p id='phone'>{information.phone}</p>
+          <p id='email'>{information.email}</p>
+          <p id='location'>{information.location}</p>
         </div>
-        <div id="user-description">{information.description}</div>
+        <div id='user-description'>{information.description}</div>
       </div>
-      <div className="rendered-experience-container">
-        <h3 id="rendered-experience-header">Work Experience</h3>
+      <div className='rendered-experience-container'>
+        <h3 id='rendered-experience-header'>Work Experience</h3>
         {experiences.map((el) => {
           return (
             <div key={el.id} id={'experience'}>
               <div>
                 <p name={'position'}>{el.position}</p>
               </div>
-              <div className="experience-info">
-                <p id="company" name={'company'}>
+              <div className='experience-info'>
+                <p id='company' name={'company'}>
                   {el.company} |
                 </p>
-                <p id="exp-start" name={'start'}>
+                <p id='exp-start' name={'start'}>
                   {el.start}-
                 </p>
-                <p id="exp-end" name={'end'}>
+                <p id='exp-end' name={'end'}>
                   {el.end}
                 </p>
               </div>
-              <div id="description-box">
-                <p id="description" name={'description'}>
+              <div id='description-box'>
+                <p id='description' name={'description'}>
                   {el.description}
                 </p>
               </div>
@@ -47,24 +47,24 @@ const CV = (props) => {
           );
         })}
       </div>
-      <div className="rendered-education-container">
-        <h3 id="rendered-education-header">Education</h3>
+      <div className='rendered-education-container'>
+        <h3 id='rendered-education-header'>Education</h3>
         {education.map((el) => {
           return (
             <div key={el.id} id={'education'}>
               <div>
-                <p id="course" name={'course'}>
+                <p id='course' name={'course'}>
                   {el.course}
                 </p>
               </div>
-              <div className="education-info">
-                <p id="university" name={'university'}>
+              <div className='education-info'>
+                <p id='university' name={'university'}>
                   {el.university} | &nbsp;
                 </p>
-                <p id="edu-start" name={'start'}>
+                <p id='edu-start' name={'start'}>
                   {el.start}-
                 </p>
-                <p id="edu-end" name={'end'}>
+                <p id='edu-end' name={'end'}>
                   {el.end}
                 </p>
               </div>
